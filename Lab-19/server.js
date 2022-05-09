f4a8b4d324c6706e5da26beb0a21e83a510763e3
Rizwanh2000 <102747057+Rizwanh2000@users.noreply.github.com>
2022-05-09 20:36:20 +0500

    Add files via upload

--- Lab-19/server.js
+++ Lab-19/server.js
@@ -1,4 +1,3 @@
-
 const express =  require("express");
 const app = express()
 const bodyParser = require("body-parser")
@@ -12,80 +11,88 @@ const db = "./db/db.json"
 
 // middlewares
 app.use(bodyParser.json())
-const { auth_me } = require("./middleware/auth");
-const auth = require("./middleware/auth");
 
-app.get("/read-db", auth_me, (req, res) => {
+
+
+
+app.get("/read-db", (req, res) => {
     data = JSON.parse(fs.readFileSync(db, "utf-8"))    
     res.json(data)
 })
 
-
-app.get("/findbyid-db/:id", auth_me,(req, res) => {
+app.get("/find-db/:name", (req, res) => {
     let data = JSON.parse(fs.readFileSync(db, "utf-8"))    
-    const _id = req.params.id;
+    const name = req.params.name;
 
-    founddata = data.find(dt => {
-        return dt.id == _id;
+    founddata = data.find(user => {
+        return user.name == name
     })
 
     console.log(typeof founddata);
 
-    res.json( typeof founddata !== "undefined" ? founddata : "Data not found")
+    res.json( typeof founddata !== "undefined" ? founddata : "User not found")
 })
 
-app.get("/findbyheader-db/:header", (req, res) => {
-    let data = JSON.parse(fs.readFileSync(db, "utf-8"))    
-    const _header = req.params.header;
-    console.log(_header);
-    
+app.get("/update-user/:name", (req, res) => {
+    let data = JSON.parse(fs.readFileSync(db, "utf-8"))  
     updatedData = []
 
     // searching user
-    data.forEach((dt, i) => {
-        if (dt.header.substring(0, _header.length) == _header){
-            updatedData.push(dt);
-        } 
-    })
-
-    console.log(typeof updatedData);
-
-    res.json( typeof updatedData !== "undefined" ? updatedData : "Data not found")
-})
-
-
-app.get("/add-item", (req, res) => {
-    let data = JSON.parse(fs.readFileSync(db, "utf-8"))  
-    updatedData = []
-    let success = true;
-
-    if(typeof req.query.id == "undefined"){
-        success = false;
-    }
-
-    if(typeof req.query.header == "undefined"){
-        success = false;
-    }
-
-    // searching dt
-    data.forEach((dt, i) => {
-        if (dt.id == req.query.id){
-            updatedData = [dt, i]
+    data.forEach((user, i) => {
+        if (user.name == req.params.name){
+            updatedData = [user, i]
             return true
         } 
     })
 
-    let addedData;
     // validating user found or not
-    if(updatedData.length == 0 && success){
+    if(updatedData.length > 0){
+
+        console.log("updatedData");
+        console.log(updatedData);
+        
+
+        // now updating user name if required
+        if(typeof req.query.n !== "undefined"){
+            data[updatedData[1]].name = req.query.n
+        }
+        
+        // now updating user password if required
+        if(typeof req.query.p !== "undefined"){
+            data[updatedData[1]].pass = req.query.p
+        }
+
+        fs.writeFileSync(db, JSON.stringify(data));
+
+    }
+
+    res.json(updatedData.length > 0 ? {msg: "Data updated", data: updatedData[0] } : "User not found")
+})
+
+app.get("/add-user", (req, res) => {
+    let data = JSON.parse(fs.readFileSync(db, "utf-8"))  
+    updatedData = []
+    let success = true;
+
+    if(typeof req.query.n == "undefined"){
+        success = false;
+    }
+
+    // searching user
+    data.forEach((user, i) => {
+        if (user.name == req.query.n){
+            updatedData = [user, i]
+            return true
+        } 
+    })
+
+    // validating user found or not
+    if(updatedData.length == 0){
 
         console.log("Adding Data");
-        addedData = {
-            id: req.query.id,
-            header: req.query.header,
-            description: (typeof req.query.desc !== "undefined") ? req.query.desc : "",
-            time: (typeof req.query.time !== "undefined") ? req.query.time : "",
-            venue: (typeof req.query.venue !== "undefined") ? req.query.venue : ""
+        let addedData = {
+            name: req.query.n,
+            pass: (typeof req.query.p !== "undefined") ? req.query.p : ""
         };
 
         console.log(addedData);
@@ -97,7 +104,56 @@ app.get("/add-item", (req, res) => {
         success = true;
     }
 
-    res.json(success ? {msg: "Data added", data: addedData } : "Data already found")
+    res.json(success ? {msg: "Data added", data: addedData } : "User already found")
+})
+
+app.get("/delete-user/:name", (req, res) => {
+    let data = JSON.parse(fs.readFileSync(db, "utf-8"))  
+    updatedData = []
+
+    // searching user
+    data.forEach((user, i) => {
+        if (user.name == req.params.name){
+            updatedData = [user, i]
+            return true
+        } 
+    })
+
+    // validating user found or not
+    if(updatedData.length > 0){
+
+        console.log("delete Data");
+        console.log(updatedData);
+        
+        data.pop(updatedData);
+
+        fs.writeFileSync(db, JSON.stringify(data));
+
+    }
+
+    res.json(updatedData.length > 0 ? {msg: "Data deleted", data: updatedData[0] } : "User not found")
+})
+
+app.get("/filter-user/:pass", (req, res) => {
+    let data = JSON.parse(fs.readFileSync(db, "utf-8"))  
+    updatedData = []
+
+    // searching user
+    data.forEach((user, i) => {
+        if (user.pass == req.params.pass){
+            updatedData.push(user);
+        } 
+    })
+
+    // validating user found or not
+    if(updatedData.length > 0){
+
+        console.log("User List");
+        console.log(updatedData);
+        
+    }
+
+    res.json(updatedData.length > 0 ? {msg: "Data found", data: updatedData } : "User not found")
 })
 
 app.listen(PORT, () => {
